dbf01f1cc1c859ec135f06fd40c93d9bec641083
apsk <andriy.s.polishchuk@gmail.com>
2014-05-11 02:37:06 +0300

    DP: redesign with BoundIx from new Index module.

--- DP.hs
+++ DP.hs
@@ -2,69 +2,54 @@ module DP where
 
 import Misc
 import Array
+import Indexed
 
-import Data.Array.IArray
-import Data.Array.MArray
+import Data.Array.IArray as IA
+import Data.Array.MArray as MA
+import Data.Array.Unboxed
 import Data.Array.Unsafe
+import Data.Array.IO
 
-type Stepper i = i -> Maybe i
+type Mapper i a b =
+  (i -> b) -> i -> a -> b
 
-type ContextAwareMapper i a b =
+type ChangeAwareMapper i a b =
   (i -> Maybe b) -> i -> a -> b
 
-dpMap :: (Ix i, IArray ar a) => ContextAwareMapper i a b -> Stepper i -> i -> ar i a -> Array i b
-dpMap mapper stepper ix arr = io $ do
-  let (bmin, bmax) = bounds arr
-  buff <- mkIOArray (bmin, bmax) Nothing
-  arr' <- mkIOArray (bmin, bmax) undefined
+dpMap :: (BoundIx i, IArray ar1 a, IArray ar2 b) =>
+  Mapper i a b -> Stepper i -> i -> b -> ar1 i a -> ar2 i b
+
+dpMap mapper stepper start_ix special_el ar = io $ do
+  ar' <- mkIOArray (IA.bounds ar) special_el
+  bounds <- MA.getBounds ar'
   let
+    validIx = inBounds bounds
     lookup ix
-      | ix < bmin || ix > bmax = Nothing
-      | otherwise = io $ readArray buff ix
+      | not (validIx ix) = special_el
+      | otherwise = io $ readArray ar' ix
     loop ix = do
-      let e' = mapper lookup ix (arr ! ix)
-      writeArray arr' ix e'
-      writeArray buff ix (Just e')
+      let e' = mapper lookup ix (ar IA.! ix)
+      writeArray ar' ix e'
+      maybe nop loop (stepper ix)
+  loop start_ix
+  unsafeFreeze ar'
+
+dpChangeAwareMap :: (BoundIx i, IArray ar1 a, IArray ar2 b) =>
+  ChangeAwareMapper i a b -> Stepper i -> i -> ar1 i a -> ar2 i b
+
+dpChangeAwareMap mapper stepper ix ar = io $ do
+  let bounds = IA.bounds ar
+  buf <- mkIOArray bounds Nothing
+  ar' <- mkIOArray bounds undefined
+  let
+    validIx = inBounds bounds
+    lookup ix
+      | not (validIx ix) = Nothing
+      | otherwise = io $ readArray buf ix
+    loop ix = do
+      let e' = mapper lookup ix (ar IA.! ix)
+      writeArray ar' ix e'
+      writeArray buf ix (Just e')
       maybe nop loop (stepper ix)
   loop ix
-  unsafeFreeze arr'
-
-dpMapMxRD :: IArray ar a => ContextAwareMapper Int2 a b -> ar Int2 a -> Array Int2 b
-dpMapMxRD mapper arr = dpMap mapper' stepper (1, 1) arr
-  where
-    (rows, cols) = snd $ bounds arr
-    mapper' stdLookup = mapper mxLookup where
-      mxLookup (r, c)
-        | r < 1 || c < 1 = Nothing
-        | otherwise = stdLookup (r, c)
-    stepper (i, j)
-      | j < cols  = Just (i, j+1)
-      | i < rows  = Just (i+1, 1)
-      | otherwise = Nothing
-
-{- No idea how to make this with ST even in unsafe way:
-   ST is extremely hard in such non-trivial scenarios...
-
-mkSTArray :: Ix i => (i, i) -> e -> ST s (STArray s i e)
-mkSTArray = newArray
-
-unsafePerformST :: ST s a -> a
-unsafePerformST = unsafePerformIO . unsafeSTToIO
-
-dpMap :: (Ix i, IArray a e1)
-      => ((i -> Maybe e2) -> i -> e1 -> e2)
-      -> (i -> Maybe i) -> i
-      -> a i e1 -> Array i e2
-dpMap f ixf ix arr = runSTArray $ do
-  let bounds' = bounds arr
-  buff <- mkSTArray bounds' Nothing
-  buffAsArray <- unsafeFreeze buff
-  arr' <- mkSTArray bounds' undefined
-  let loop ix = do
-        let e' = f (buffAsArray !) ix (arr ! ix)
-        writeArray arr' ix e'
-        writeArray buff ix (Just e')
-        maybe nop loop (ixf ix)
-  loop ix
-  return arr'
--}
+  unsafeFreeze ar'
