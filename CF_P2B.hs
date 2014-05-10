69003ae4d97fb1f89f676cd1b70fac782c0218ec
apsk <andriy.s.polishchuk@gmail.com>
2014-05-11 02:47:55 +0300

    CF_P2B: bump solution to recent stuff; still memory-expensive.

--- CF_P2B.hs
+++ CF_P2B.hs
@@ -1,53 +1,61 @@
 module CF_P2B where -- http://codeforces.ru/problemset/problem/2/B
 
+import Data.Word
 import Data.List
-import Data.Array
 import Control.Arrow
+import Control.Applicative
 
 import Array
+import Indexed
+import Numeric
 import Misc
 import DP
 
+type Number = Word32
+type Count = Int
+type Matrix = UArray (Int, Int) Number
 type Path = String
-type Matrix = Array Int2 Integer
 
 zeroPath :: Matrix -> Maybe Path
-zeroPath mx = fmap (pathThrough . fst) $ find ((0 ==) . snd) $ assocs mx
+zeroPath mx = pathThrough <$> findIx is0 mx
   where
-    (rows, cols) = snd $ bounds mx
-    pathThrough (r, c) =
-      replicate (c - 1) 'R' ++
-      replicate (rows - 1) 'D' ++
-      replicate (cols - c) 'R'
+    (r, c) = upperBound mx
 
-minDivisiblePath :: Matrix -> Int -> (Int, Path)
-minDivisiblePath mx n = mapSnd reverse $ dpMapMxRD mapper mx ! (rows, cols)
+    pathThrough (ri, ci) = replicate (ci - 1) 'R' ++
+      replicate (r - 1) 'D' ++ replicate (c - ci) 'R'
+
+minDivisiblePath :: Number -> Matrix -> (Count, Path)
+minDivisiblePath n mx = mapSnd reverse (dpMx ! (r, c))
   where
-    (rows, cols) = snd $ bounds mx
-    mapper lookup ix e = case bi lookup (ix -. 1, ix .- 1) of
-      (Nothing, Nothing) -> (dc, "")
-      (Just (c, p), Nothing) -> (c + dc, 'R' : p)
-      (Nothing, Just (c, p)) -> (c + dc, 'D' : p)
-      (Just (c1, p1), Just (c2, p2)) ->
-        minBy fst (c1 + dc, 'R' : p1) (c2 + dc, 'D' : p2)
+    (r, c) = upperBound mx
+
+    dpMx :: Array (Int, Int) (Count, Path)
+    dpMx = dpChangeAwareMap mapper (mxRD r c) (1, 1) mx
+
+    mapper lookup ix e =
+      mb2u (dc, "") fR fD (minBy fst .% (fR *** fD)) $
+        bi lookup (ix -. 1, ix .- 1)
       where
-        dc = timesDivisibleBy (toi n) e
+        dc = timesDivisibleBy (fi n) e
+        fR = (dc +) *** ('R' :)
+        fD = (dc +) *** ('D' :)
 
-solveDP :: Matrix -> (Int, Path)
+solveDP :: Matrix -> (Count, Path)
 solveDP mx = maybe cp25 (\p0 -> minBy fst cp25 (1, p0)) (zeroPath mx)
   where
-    cp25 = uncurry (minBy fst) $ bi (minDivisiblePath mx) (2, 5)
+    cp25 = minBy fst $% bi (flip minDivisiblePath mx) (2, 5)
 
 main = do
   dim <- readLn
-  mx <- mxRead dim dim
+  mx <- mxReadU dim dim
   let (c, p) = solveDP mx
   print c
   putStrLn p
 
 -- At first I've came with this solution, but then realized
--- it's actually not optimal. This is a greedy algorithm, not DP.
+-- it's actually not optimal. This is a greedy algorithm, not DP:
 
+{-
 cost Nothing             = 0
 cost (Just (d5, d2, d0)) = min d5 d2 + d0
 
@@ -69,3 +77,4 @@ solveGreedy mx = cost *** reverse $ dpMapMxRD mapper mx ! (rows, cols)
           | e `mod` 5  == 0 = Just (d5 + 1, d2, d0)
           | e `mod` 2  == 0 = Just (d5, d2 + 1, d0)
           | otherwise       = Just c
+-}
